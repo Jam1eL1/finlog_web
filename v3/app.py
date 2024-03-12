44945e84db29bcc8ec307607da89d60a2b421e54
Jam1eL1 <23jisu@gmail.com>
2024-03-12 12:15:44 -0700

    app.py - Created entries list for saving  user form input data and datetime. - Changed date format from ymd to bd(Month abbreviated, date) - Modified home.html to reflect recent posts when user submits form data

--- v3/app.py
+++ v3/app.py
@@ -9,9 +9,20 @@ def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date =  datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
-    return render_template("home.html")
-
+        # print(entry_content, formatted_date)
+        entries.append((entry_content, formatted_date))
+    
+    entries_with_date = [ 
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d") 
+        )
+            for entry in entries
+    ]
+    
+    return render_template("home.html", entries = entries_with_date)
+    
 if __name__ == "__main__":
     # if receiving user data
     app.run(debug=True)
