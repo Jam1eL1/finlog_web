89d52ecfef64fd21e3c44ec8889556eeec591cce
Jam1eL1 <23jisu@gmail.com>
2024-03-18 17:33:53 -0700

    Implemented MongoDB to improve the way we save user form submission and retrieving data onto the webpage

--- v3/app.py
+++ v3/app.py
@@ -1,28 +1,38 @@
 import datetime
 from flask import Flask, render_template, request
-app = Flask(__name__)
+from pymongo import MongoClient
 
-entries = []
+def create_app():
+    app = Flask(__name__)
+# set up client (represents the cluster you set up)
+    client = MongoClient('mongodb+srv://test_user:LtcatrLRt7RCJY4N@cluster0.ckfhg1z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    # set up db
+    app.db = client.finlog
+# dbs = client.list_database_names() to see dbs existing inside the cluster
+    entries = []
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date =  datetime.datetime.today().strftime("%Y-%m-%d")
-        # print(entry_content, formatted_date)
-        entries.append((entry_content, formatted_date))
-    
-    entries_with_date = [ 
-        (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d") 
-        )
-            for entry in entries
-    ]
-    
-    return render_template("home.html", entries = entries_with_date)
-    
-if __name__ == "__main__":
-    # if receiving user data
-    app.run(debug=True)
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        # print(dbs)
+        # print([e for e in app.db.entries.find({})])
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date =  datetime.datetime.today().strftime("%Y-%m-%d")
+            # print(entry_content, formatted_date)
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+        
+        entries_with_date = [ 
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d") 
+            )
+                for entry in app.db.entries.find({})
+        ]
+        
+        return render_template("home.html", entries = entries_with_date)
+        
+    # if __name__ == "__main__":
+    #     # if receiving user data
+    #     app.run(debug=True)
+    return app
